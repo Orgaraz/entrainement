08b804dd142f88690df15c5e491e235df0e8a1a5
Orgaraz <zoecrasnier@gmail.com>
2024-07-24 17:56:39 +0200

    envoifonctiontest2.2

--- test2.py
+++ test2.py
@@ -1,1 +1,7 @@
-fdyfuhuijhuoipjopij # type: ignore
+fruits = ["pomme", "banane", "orange"]
+nouveau_fruits = ["kiwi", "café", "tomate"]
+fruits = fruits + nouveau_fruits
+fruits.remove("orange")
+fruits[1] = "ananas"
+fruits.sort()
+print(fruits)
